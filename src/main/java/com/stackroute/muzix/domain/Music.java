b9a6d594072ace734df70f410f598f417ff1a915
cazzprogrammer <dminj97@gmail.com>
2019-10-17 09:53:57 +0530

    first- commit : Lombok Addition

--- src/main/java/com/stackroute/muzix/domain/Music.java
+++ src/main/java/com/stackroute/muzix/domain/Music.java
@@ -1,54 +1,20 @@
 package com.stackroute.muzix.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Music {
     @Id
     int trackId;
     String trackName;
     String comments;
 
-    public Music(int trackId, String trackName, String comments) {
-        this.trackId = trackId;
-        this.trackName = trackName;
-        this.comments = comments;
-    }
-
-    public Music() {
-    }
-
-    public int getTrackId() {
-        return trackId;
-    }
-
-    public void setTrackId(int trackId) {
-        this.trackId = trackId;
-    }
-
-    public String getTrackName() {
-        return trackName;
-    }
-
-    public void setTrackName(String trackName) {
-        this.trackName = trackName;
-    }
-
-    public String getComments() {
-        return comments;
-    }
-
-    public void setComments(String comments) {
-        this.comments = comments;
-    }
-
-    @Override
-    public String toString() {
-        return "Music{" +
-                "trackId=" + trackId +
-                ", trackName='" + trackName + '\'' +
-                ", comments='" + comments + '\'' +
-                '}';
-    }
 }
