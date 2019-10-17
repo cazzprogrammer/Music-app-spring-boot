77aee67a6232f8f8877274a209b7acbba094cff7
cazzprogrammer <dminj97@gmail.com>
2019-10-17 14:06:20 +0530

    first- commit : pass value using @VAlue  Annotation And Environment.

--- src/main/java/com/stackroute/muzix/seeder/Listener.java
+++ src/main/java/com/stackroute/muzix/seeder/Listener.java
@@ -3,13 +3,24 @@ package com.stackroute.muzix.seeder;
 import com.stackroute.muzix.domain.Music;
 import com.stackroute.muzix.service.MusicService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Listener implements ApplicationListener<ContextRefreshedEvent> {
     MusicService musicService;
+
+    @Value("${id}")
+    int id;
+    @Value("${name}")
+    String name;
+    @Value("${comment}")
+    String comment;
+    @Autowired
+    Environment environment;
 
     @Autowired
     public Listener(MusicService musicService) {
@@ -20,5 +31,7 @@ public class Listener implements ApplicationListener<ContextRefreshedEvent> {
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
 
         musicService.saveUserListen(new Music(3,"sas","sads"));
+        musicService.saveUserListen(new Music(id,name,comment));
+        musicService.saveUserListen(new Music(Integer.parseInt((environment.getProperty("id1"))),environment.getProperty("name1"),environment.getProperty("comment1")));
     }
 }
