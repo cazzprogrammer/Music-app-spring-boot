96e09d6cf0437122f543b1de843452f0a6b5a9e0
cazzprogrammer <dminj97@gmail.com>
2019-10-18 11:30:10 +0530

    Using mongpdb

--- src/main/java/com/stackroute/muzix/repository/MusicRepository.java
+++ src/main/java/com/stackroute/muzix/repository/MusicRepository.java
@@ -1,15 +1,15 @@
 package com.stackroute.muzix.repository;
 
 import com.stackroute.muzix.domain.Music;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface MusicRepository extends JpaRepository<Music,Integer> {
+public interface MusicRepository extends MongoRepository<Music,Integer> {
 
-     @Query("FROM Music WHERE trackName= ?1")
+     @Query("{'trackName' : ?0}")
      List<Music> getMusicInfoByName(String trackName);
 }
